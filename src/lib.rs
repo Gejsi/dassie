f845415da5d777429a7211354df1eb2fec9884f9
Gejsi <vgejsi@gmail.com>
2023-08-22 16:59:04 +0200

    Closed selector parenthesis during parsing

--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,55 @@
 use cssparser::{ParseError, Parser as Lexer, ParserInput as LexerInput, Token};
 
 fn parse(lexer: &mut Lexer, selector: &mut String) -> anyhow::Result<()> {
-    while let Ok(token) = lexer.next() {
+    while let Ok(token) = lexer.next_including_whitespace() {
         match token {
             Token::Delim(value) => selector.push_str(&value.to_string()),
             Token::WhiteSpace(value) => selector.push_str(value),
-            Token::Comma => selector.push_str(", "),
+            Token::Comma => selector.push_str(","),
+
             Token::CurlyBracketBlock => {
                 println!("{selector}");
                 selector.clear();
             }
+
             Token::Ident(value)
             | Token::Hash(value)
             | Token::IDHash(value)
-            | Token::QuotedString(value)
-            | Token::UnquotedUrl(value)
-            | Token::Function(value) => selector.push_str(value),
-            Token::AtKeyword(value) => selector.push_str(&format!("@{value} ")),
-            Token::ParenthesisBlock => selector.push_str("("),
+            | Token::UnquotedUrl(value) => selector.push_str(value),
+            Token::AtKeyword(value) => selector.push_str(&format!("@{value}")),
+            Token::QuotedString(value) => selector.push_str(&format!("'{value}'")),
+
+            Token::ParenthesisBlock => {
+                selector.push_str("(");
+                lexer
+                    .parse_nested_block(|inner_lexer| {
+                        let _ = parse(inner_lexer, selector);
+                        selector.push_str(")");
+                        Ok::<(), ParseError<anyhow::Error>>(())
+                    })
+                    .unwrap();
+            }
             Token::SquareBracketBlock => {
                 selector.push_str("[");
                 lexer
                     .parse_nested_block(|inner_lexer| {
                         let _ = parse(inner_lexer, selector);
+                        selector.push_str("]");
                         Ok::<(), ParseError<anyhow::Error>>(())
                     })
                     .unwrap();
             }
-            Token::CloseParenthesis => selector.push_str(")"),
-            Token::CloseSquareBracket => selector.push_str("]"),
+            Token::Function(value) => {
+                selector.push_str(&format!("{value}("));
+                lexer
+                    .parse_nested_block(|inner_lexer| {
+                        let _ = parse(inner_lexer, selector);
+                        selector.push_str(")");
+                        Ok::<(), ParseError<anyhow::Error>>(())
+                    })
+                    .unwrap();
+            }
+
             Token::Colon => selector.push_str(":"),
             Token::Semicolon => selector.push_str(";"),
             Token::IncludeMatch => selector.push_str("~="),
